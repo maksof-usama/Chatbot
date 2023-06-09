20b626ceeb4d73ce420430beb368b7e2bf9fd5aa
maksof-usama <85098979+maksof-usama@users.noreply.github.com>
2023-06-09 15:08:48 +0500

    COMMIT-ALL-DONE

--- src/app/pages/chat/chat-container.component.ts
+++ src/app/pages/chat/chat-container.component.ts
@@ -1,38 +1,50 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { CHAT_TYPE } from '~/app/pages/chat/chat.component';
-import * as MarkDownIt from 'markdown-it'
+import * as MarkDownIt from 'markdown-it';
 @Component({
   selector: 'app-chat-container',
   template: `
-    <div class="chat-container">
-            <div *ngFor="let chat of chats"  >
-                <div class='questoin' [ngClass]="{'answer' : chat.type == 'ANSWER'}"  >
-                    <div class="text-mesg">
-                        <div class="user-icon">
-                            <span><img src="https://storage.googleapis.com/dawaadost.appspot.com/medwiki-videos/medwiki-assets/user.png" alt="user-icon"></span> 
-                        </div>
-                        <div class="mesg-main">
-                        <div class="mesg">
-                            <p [innerHTML]="md.render(chat.text)"></p>
-          
-                          </div>
-                        </div>
-                       
-                      </div>
-                </div>
+    <div class="chat-container" id="containerElement">
+      <div *ngFor="let chat of chats">
+        <div class="questoin" [ngClass]="{ answer: chat.type == 'ANSWER' }">
+          <div class="text-mesg">
+            <div class="user-icon">
+              <span
+                ><img
+                  *ngIf="chat.type == 'ANSWER'"
+                  src="https://storage.googleapis.com/dawaadost.appspot.com/medwiki-videos/medwiki-assets/user.png"
+                  alt="user-icon"
+                />
+                <img
+                  *ngIf="chat.type == 'QUESTION'"
+                  src="../../../assets/img/user.png"
+                  alt="user-icon"
+                />
+              </span>
             </div>
-             <a href='/' *ngIf="finalMessage">Link</a>
+            <div class="mesg-main">
+              <div class="mesg">
+                <p [innerHTML]="md.render(chat.text)"></p>
+              </div>
+            </div>
+          </div>
         </div>
-  `
+      </div>
+      <a href="/" *ngIf="finalMessage">Link</a>
+      <div id="chatscroll"></div>
+    </div>
+  `,
 })
 export class ChatContainerComponent implements OnInit {
-  @Input() chats: CHAT_TYPE[] = []
-  @Input() finalMessage: boolean = false
-  constructor() { }
+  @Input() chats: CHAT_TYPE[] = [];
+  @Input() finalMessage: boolean = false;
+  constructor() {}
   md: MarkDownIt = new MarkDownIt({
     html: true,
-    linkify: true
-  })
-  ngOnInit(): void { }
-
+    linkify: true,
+  });
+  ngOnInit(): void {}
 }
+
+// chat.type == 'ANSWER' == user
+// chat.type == 'QUESTION' == doctor
--- src/app/pages/chat/chat.component.ts
+++ src/app/pages/chat/chat.component.ts
@@ -1,59 +1,63 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  AfterViewChecked,
+  ElementRef,
+  ViewChild,
+} from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { askQuestion, login, verifyOtp } from '~/lib/api';
 import { USER_KEY, session } from '~/lib/storage';
 import { preDefinedMessages, validateEmail } from '~/lib/utils';
-export type CHAT_TYPE = { text: string; type: "QUESTION" | "ANSWER" };
+export type CHAT_TYPE = { text: string; type: 'QUESTION' | 'ANSWER' };
 @Component({
   selector: 'app-chat',
   templateUrl: './chat.component.html',
-  styleUrls: ['./chat.component.scss']
+  styleUrls: ['./chat.component.scss'],
 })
 export class ChatComponent implements OnInit {
-  chats: CHAT_TYPE[] = []
-  user: Record<string, any> | null = this.userInfo || null
+  chats: CHAT_TYPE[] = [];
+  user: Record<string, any> | null = this.userInfo || null;
   noData: boolean = false;
-  submitted: boolean = false
+  submitted: boolean = false;
   isLoading: boolean = false;
   isOTPVerificationRequired: boolean = false;
   chatForm = new FormGroup({
-    question: new FormControl('', [Validators.required])
-  })
+    question: new FormControl('', [Validators.required]),
+  });
   socket;
   finalMessage: boolean = false;
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
     this.isOTPVerificationRequired = this.user?.['isNewUser'];
-    this.pushChat(preDefinedMessages[0])
-    this.socket = window?.['io']("wss://app.medwiki.co.in", {
-      transports: ["websocket"],
-      path: "/node"
+    this.pushChat(preDefinedMessages[0]);
+    this.socket = window?.['io']('wss://app.medwiki.co.in', {
+      transports: ['websocket'],
+      path: '/node',
     });
-    this.socket.on("QUERY_RESPONE_VISITOR", ({
-      text,
-      visitorId
-    }) => {
+    this.socket.on('QUERY_RESPONE_VISITOR', ({ text, visitorId }) => {
       const userId = this.user?.['id'];
       if (visitorId == userId) {
         this.pushChat(text);
         setTimeout(() => {
-          this.pushChat(preDefinedMessages[5])
-          this.finalMessage = true
-        }, 3000)
+          this.pushChat(preDefinedMessages[5]);
+          this.finalMessage = true;
+        }, 3000);
       }
-    })
+    });
   }
   async submitHandler() {
     if (this.isLoading) return;
     this.chatForm.markAllAsTouched();
     this.submitted = true;
-    const val = this.chatForm.value['question']
+    const val = this.chatForm.value['question'];
     if (!this.chatForm.valid) return;
-    this.chatForm.reset()
-    this.pushChat(val, false)
+    this.chatForm.reset();
+    this.scrollToBottom();
+    this.pushChat(val, false);
     try {
-      this.isLoading = true
+      this.isLoading = true;
       if (!this.user) {
         if (!validateEmail(val)) {
           throw new Error(preDefinedMessages[1]);
@@ -62,15 +66,20 @@ export class ChatComponent implements OnInit {
         this.user = response;
 
         if (this.user?.['isNewUser']) {
-          this.isOTPVerificationRequired = true
+          this.isOTPVerificationRequired = true;
           this.pushChat(preDefinedMessages[2]);
-          return
+          return;
         }
         if (response.credit >= response.maxCredit) {
-          this.user = null
+          this.user = null;
           throw new Error(preDefinedMessages[3]);
         }
-        this.pushChat(preDefinedMessages[4].replace('Thank you for your email verification, ', ''));
+        this.pushChat(
+          preDefinedMessages[4].replace(
+            'Thank you for your email verification, ',
+            ''
+          )
+        );
         session.set(USER_KEY, response);
         return;
       }
@@ -81,11 +90,11 @@ export class ChatComponent implements OnInit {
         });
         this.user = response;
         if (response.credit >= response.maxCredit) {
-          this.user = null
+          this.user = null;
           throw new Error(preDefinedMessages[3]);
         }
         this.pushChat(preDefinedMessages[4]);
-        this.isOTPVerificationRequired = false
+        this.isOTPVerificationRequired = false;
         session.set(USER_KEY, response);
         return;
       }
@@ -93,29 +102,30 @@ export class ChatComponent implements OnInit {
       this.pushChat(response.message);
     } catch (err: any) {
       if (err.status === 401) {
-        session.clear(USER_KEY)
-        this.user = null
+        session.clear(USER_KEY);
+        this.user = null;
       }
       this.pushChat(err?.json?.message || err.message);
     } finally {
       this.isLoading = false;
     }
-
-
-
-
   }
   pushChat(text, answer = true) {
     this.chats.push({
       text,
-      type: answer ? 'ANSWER' : "QUESTION"
-    })
-    const chatBody = document.getElementById("chat-container");
-    if (chatBody) {
-      chatBody.scrollTop = chatBody.scrollHeight;
-    }
+      type: answer ? 'ANSWER' : 'QUESTION',
+    });
+    this.scrollToBottom();
   }
   get userInfo() {
     return session.get(USER_KEY);
+  }
+  scrollToBottom() {
+    const chatBody = document.getElementById('chatscroll');
+    if (chatBody) {
+      setTimeout(() => {
+        chatBody.scrollIntoView();
+      }, 50);
+    }
   }
 }
